e80c4437f79264ea1144ef742cfbc6869fec23e0
jaehe <jaehe0413@naver.com>
2025-01-06 15:35:50 +0900

    1.4 statement 쪼개기 - totalAmount 문장 슬라이드

--- ch01/after.js
+++ ch01/after.js
@@ -58,7 +58,6 @@ function statement(invoice, plays) {
     return volumeCredits;
   }
 
-  let totalAmount = 0;
   let result = `청구 내역 (고객명): ${invoice.customer}\n`;
 
   for (let perf of invoice.performances) {
@@ -68,6 +67,7 @@ function statement(invoice, plays) {
     }석)\n`;
   }
 
+  let totalAmount = 0;
   for (let perf of invoice.performances) {
     totalAmount += amountFor(perf);
   }
