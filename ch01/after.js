0e9d31881150e1aad51781b6b3e3bc11defed65b
jaehe <jaehe0413@naver.com>
2025-01-06 11:21:19 +0900

    1.4 statement 쪼개기 - format 추출

--- ch01/after.js
+++ ch01/after.js
@@ -1,3 +1,12 @@
+// statement() 함수...
+function format(aNumber) {
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2,
+  }).format(aNumber);
+}
+
 function statement(invoice, plays) {
   // statement() 함수... 연극의 가격을 계산한다.
   function amountFor(aPerformance) {
@@ -43,11 +52,6 @@ function statement(invoice, plays) {
   let totalAmount = 0;
   let volumeCredits = 0;
   let result = `청구 내역 (고객명): ${invoice.customer}\n`;
-  const format = new Intl.NumberFormat("en-US", {
-    style: "currency",
-    currency: "USD",
-    minimumFractionDigits: 2,
-  }).format;
 
   for (let perf of invoice.performances) {
     volumeCredits += volumeCreditsFor(perf);
