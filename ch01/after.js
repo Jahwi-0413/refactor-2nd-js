01160c06b6f70d14f7780faa9ef2759d57285785
jaehe <jaehe0413@naver.com>
2025-01-06 10:15:57 +0900

    1.4 statement 쪼개기 - amountFor

--- ch01/after.js
+++ ch01/after.js
@@ -1,15 +1,6 @@
 function statement(invoice, plays) {
-  let totalAmount = 0;
-  let volumeCredits = 0;
-  let result = `청구 내역 (고객명): ${invoice.customer}\n`;
-  const format = new Intl.NumberFormat("en-US", {
-    style: "currency",
-    currency: "USD",
-    minimumFractionDigits: 2,
-  }).format;
-
-  for (let perf of invoice.performances) {
-    const play = plays[perf.playID];
+  // statement() 함수... 연극의 가격을 계산한다.
+  function amountFor(perf, play) {
     let thisAmount = 0;
 
     switch (play.type) {
@@ -29,6 +20,21 @@ function statement(invoice, plays) {
       default:
         throw new Error(`알 수 없는 장르: ${play.type}`);
     }
+    return thisAmount;
+  }
+
+  let totalAmount = 0;
+  let volumeCredits = 0;
+  let result = `청구 내역 (고객명): ${invoice.customer}\n`;
+  const format = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2,
+  }).format;
+
+  for (let perf of invoice.performances) {
+    const play = plays[perf.playID];
+    let thisAmount = amountFor(perf, play);
 
     // 포인트를 정립한다.
     volumeCredits += Math.max(perf.audience - 30, 0);
