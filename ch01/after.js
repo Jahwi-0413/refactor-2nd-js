e4fdef4e289e4e6266394c3ed1136f1444d162e3
jaehe <jaehe0413@naver.com>
2025-01-06 11:13:29 +0900

    1.4 statement 쪼개기 - volumeCreditsFor

--- ch01/after.js
+++ ch01/after.js
@@ -28,6 +28,18 @@ function statement(invoice, plays) {
     return plays[perf.playID];
   }
 
+  // statement() 함수...
+  function volumeCreditsFor(perf) {
+    let volumeCredits = 0;
+    // 포인트를 정립한다.
+    volumeCredits += Math.max(perf.audience - 30, 0);
+    // 희극 관객 5명마다 추가 포인트를 제공한다.
+    if ("comedy" === playFor(perf).type)
+      volumeCredits += Math.floor(perf.audience / 5);
+
+    return volumeCredits;
+  }
+
   let totalAmount = 0;
   let volumeCredits = 0;
   let result = `청구 내역 (고객명): ${invoice.customer}\n`;
@@ -38,11 +50,7 @@ function statement(invoice, plays) {
   }).format;
 
   for (let perf of invoice.performances) {
-    // 포인트를 정립한다.
-    volumeCredits += Math.max(perf.audience - 30, 0);
-    // 희극 관객 5명마다 추가 포인트를 제공한다.
-    if ("comedy" === playFor(perf).type)
-      volumeCredits += Math.floor(perf.audience / 5);
+    volumeCredits += volumeCreditsFor(perf);
 
     // 청구 내역을 출력한다.
     result += `  ${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${
