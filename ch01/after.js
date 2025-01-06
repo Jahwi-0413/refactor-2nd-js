23ac18fabef26bb47ac44b95b6ea9eb2629deded
jaehe <jaehe0413@naver.com>
2025-01-06 10:57:21 +0900

    1.4 statement 쪼개기 - playFor 인라인

--- ch01/after.js
+++ ch01/after.js
@@ -23,6 +23,7 @@ function statement(invoice, plays) {
     return result;
   }
 
+  // statement() 함수...
   function playFor(perf) {
     return plays[perf.playID];
   }
@@ -37,16 +38,16 @@ function statement(invoice, plays) {
   }).format;
 
   for (let perf of invoice.performances) {
-    const play = playFor(perf);
-    let thisAmount = amountFor(perf, play);
+    let thisAmount = amountFor(perf, playFor(perf));
 
     // 포인트를 정립한다.
     volumeCredits += Math.max(perf.audience - 30, 0);
     // 희극 관객 5명마다 추가 포인트를 제공한다.
-    if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
+    if ("comedy" === playFor(perf).type)
+      volumeCredits += Math.floor(perf.audience / 5);
 
     // 청구 내역을 출력한다.
-    result += `  ${play.name}: ${format(thisAmount / 100)} (${
+    result += `  ${playFor(perf).name}: ${format(thisAmount / 100)} (${
       perf.audience
     }석)\n`;
     totalAmount += thisAmount;
