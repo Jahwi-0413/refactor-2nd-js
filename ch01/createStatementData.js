5f3a3b56d2718edce2ca12e1e6e535873e57968e
jaehe <jaehe0413@naver.com>
2025-01-07 15:21:23 +0900

    1.8 다형성을 활용해 계산 코드 재구성하기 - volumeCredits 계산기로 옮김

--- ch01/createStatementData.js
+++ ch01/createStatementData.js
@@ -29,14 +29,8 @@ function createStatementData(invoice, plays) {
   }
 
   function volumeCreditsFor(aPerformance) {
-    let result = 0;
-    // 포인트를 정립한다.
-    result += Math.max(aPerformance.audience - 30, 0);
-    // 희극 관객 5명마다 추가 포인트를 제공한다.
-    if ("comedy" === aPerformance.play.type)
-      result += Math.floor(aPerformance.audience / 5);
-
-    return result;
+    return new PerformanceCalculator(aPerformance, playFor(aPerformance))
+      .volumeCredits;
   }
 
   function totalAmount(data) {
@@ -74,6 +68,17 @@ class PerformanceCalculator {
       default:
         throw new Error(`알 수 없는 장르: ${this.performance.play.type}`);
     }
+    return result;
+  }
+
+  get volumeCredits() {
+    let result = 0;
+    // 포인트를 정립한다.
+    result += Math.max(this.performance.audience - 30, 0);
+    // 희극 관객 5명마다 추가 포인트를 제공한다.
+    if ("comedy" === this.performance.play.type)
+      result += Math.floor(this.performance.audience / 5);
+
     return result;
   }
 }
