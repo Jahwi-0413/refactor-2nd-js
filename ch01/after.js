f281473ade26c1d989e9d5b8b3d03ed64037524a
jaehe <jaehe0413@naver.com>
2025-01-07 14:29:54 +0900

    1.6 계산 단계와 포맷팅 단계 분리하기 - htmlStatement

--- ch01/after.js
+++ ch01/after.js
@@ -25,4 +25,26 @@ function renderPlainText(data, plays) {
   return result;
 }
 
+function htmlStatement(invoices, plays) {
+  return renderHtml(createStatementData(invoices, plays));
+}
+
+function renderHtml(data) {
+  let result = `<h1>청구 내역 (고객명): ${data.customer}</h1>\n`;
+  result += "<table>\n";
+  result += "<tr><th>연극</th><th>좌석 수</th><th>금액</th></tr>\n";
+
+  for (let perf of data.performances) {
+    // 청구 내역을 출력한다.
+    result += `   <tr><td>${perf.play.name}</td>`;
+    result += `<td>(${perf.audience}석)</td>`;
+    result += `<td>${usd(perf.amount)}</td></tr>\n`;
+  }
+
+  result += "</table>\n";
+  result += `<p>총액: <em>${usd(data.totalAmount)}</em></p>\n`;
+  result += `<p>적립 포인트: <em>${data.totalVolumeCredits}</em>점</p>\n`;
+  return result;
+}
+
 module.exports = { statement };
